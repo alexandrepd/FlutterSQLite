e8bb054527619fc78b2e5647a75142e295a72365
Alexandre Alves Silva de Oliveira <alexandre.oliveira@restoque.com.br>
2020-06-22 12:36:20 -0300

    Dog Model

--- lib/model/dog.dart
+++ lib/model/dog.dart
@@ -1,9 +1,19 @@
 class Dog {
-
   final int id;
   final String name;
   final int age;
 
-  Dog(this.id, this.name, this.age);
-  
+  Dog({this.id, this.name, this.age});
+
+  factory Dog.fromDatabaseJson(Map<String, dynamic> data) {
+    return Dog(id: data['id'], name: data['name'], age: data['age']);
+  }
+
+  Map<String, dynamic> toDatabaseJson() {
+    return {
+      "id": this.id,
+      "name": this.name,
+      "age": this.age,
+    };
+  }
 }
