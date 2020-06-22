84e2a37f86f01c27590ad5fc66d40e4c104ff63a
Alexandre Alves Silva de Oliveira <alexandre.oliveira@restoque.com.br>
2020-06-22 13:45:57 -0300

    adding controller

---
+++ lib/controller/dog_controller.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:FlutterSQLite/model/dog.dart';
+import 'package:FlutterSQLite/repository/dog_repository.dart';
+
+class DogController {
+  final _dogRepository = DogRepository();
+
+  final _dogController = StreamController<List<Dog>>.broadcast();
+
+  getDogs({String query}) async {
+    _dogController.sink.add(await _dogRepository.getAllDogs(query: query));
+  }
+
+  insertDog(Dog dog) {
+    _dogRepository.insertDog(dog);
+    getDogs();
+  }
+
+  updateDog(Dog dog) {
+    _dogRepository.updateDog(dog);
+    getDogs();
+  }
+
+  deleteDog(Dog dog) {
+    _dogRepository.deleteDog(dog);
+    getDogs();
+  }
+
+  deleteDogById(int id) {
+    _dogRepository.deleteDogById(id);
+    getDogs();
+  }
+
+  deleteAllDogs() {
+    _dogRepository.deleteAllDogs();
+    getDogs();
+  }
+
+  dispose() {
+    _dogController.close();
+  }
+}
